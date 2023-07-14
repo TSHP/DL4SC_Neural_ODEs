696b2852d79a9d46239499653c0db8acf544e998
TSHP <tshpakov@hotmail.com>
2023-07-14 14:50:09 +0200

    Try CE with one channel lesse

--- src/training/segmentation_training_module.py
+++ src/training/segmentation_training_module.py
@@ -16,13 +16,14 @@ class SegmentationTrainingModule(ABCTrainingModule):
 
     def compute_loss(self, inputs, masks):
         out = self.model(inputs)
-        return out, self.loss(out, masks)
+        return out, self.loss(out, torch.argmax(masks, dim=1))
 
     def compute_mIoU(self, predictions, masks):
         return self.jaccard(torch.argmax(predictions, dim=1), torch.argmax(masks, dim=1))
 
     def compute_metrics(self, predictions, masks):
-        batches = self.last_test_image_batch.shape[0] // 6
+        length = self.last_test_image_batch.shape[0]
+        batches = length // 6
         for i in range(batches):
-            vizualize_segmentation_predictions(self.output_path / f"epoch_{self.epoch}_mask_gt_p{i}.png", self.last_test_image_batch[6 * i:(i + 1) * 6].cpu().numpy(), torch.argmax(masks[6 * i:(i + 1) * 6], dim=1).cpu().numpy(), torch.argmax(predictions[6 * i:(i + 1) * 6], dim=1).cpu().numpy())
+            vizualize_segmentation_predictions(self.output_path / f"epoch_{self.epoch}_mask_gt_p{i}.png", self.last_test_image_batch[6 * i:(i + 1) * 6].cpu().numpy(), torch.argmax(masks[length:][6 * i:(i + 1) * 6], dim=1).cpu().numpy(), torch.argmax(predictions[length:][6 * i:(i + 1) * 6], dim=1).cpu().numpy())
         return {"mIoU": self.compute_mIoU(predictions, masks)}
